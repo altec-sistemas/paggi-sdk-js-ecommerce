c925ccab6b28b4aa4b69c453c95bb48137da7830
= <=>
2018-10-18 08:01:51 -0300

    Passagem de testes

--- SDK/tokenValidation.js
+++ SDK/tokenValidation.js
@@ -1,1 +1,31 @@
-class TokenValidation {}
+const jwtDecode = require("jwt-decode");
+
+const TokenValidation = {}; // C Object simplifies exporting the module
+
+TokenValidation.isValid = token => {
+  let isPartnerSet;
+  try {
+    const decodedToken = jwtDecode(token);
+    isPartnerSet =
+      typeof decodedToken.permissions[0].partner_id !== "undefined";
+  } catch (err) {
+    isPartnerSet = false;
+  } finally {
+    return isPartnerSet;
+  }
+};
+
+TokenValidation.isExpired = token => {
+  const decodedToken = jwtDecode(token);
+  const date = new Date(decodedToken.exp);
+  todayDate = new Date().valueOf() / 1000;
+  return +date < +todayDate;
+};
+
+TokenValidation.isExpiring = token => {
+  const decodedToken = jwtDecode(token);
+  const date = new Date(decodedToken.exp * 1000);
+  todayDate = new Date().valueOf();
+  return +todayDate > +date - 2592000;
+};
+module.exports = TokenValidation;
