f0f1175a7f450ae134e9ca8769dffa133aaa7b21
= <=>
2018-10-18 08:01:21 -0300

    Correção de testes

--- test/tokenValidationTest.js
+++ test/tokenValidationTest.js
@@ -1,37 +1,38 @@
 const chai = require("chai");
-const TokenValidation = require("../SDK/tokenValidation");
+const TokenValidator = require("../SDK/tokenValidation");
 
 const token = process.env.TOKEN;
 const invalidToken = process.env.INVALIDTOKEN;
 const expiredToken = process.env.EXPIREDTOKEN;
 const expiringToken = process.env.EXPIRINGTOKEN;
 
-/* global describe it */
 describe("Token Validation", () => {
   describe("#isValid()", () => {
     it("Should return true if token is valid", () => {
-      chai.assert.isTrue(TokenValidation.isValid(token));
+      const valid = TokenValidator.isValid(token);
+      chai.assert.isTrue(valid);
     });
     it("Should return false if token is invalid", () => {
-      chai.assert.isTrue(TokenValidation.isValid(invalidToken));
+      const valid = TokenValidator.isValid(invalidToken);
+      chai.assert.isFalse(valid);
     });
   });
 
   describe("#isExpired()", () => {
     it("Should return true if token is expired", () => {
-      chai.assert.isTrue(TokenValidation.isExpired(expiredToken));
+      chai.assert.isTrue(TokenValidator.isExpired(expiredToken));
     });
     it("Should return false if token is not expired", () => {
-      chai.assert.isTrue(TokenValidation.isExpired(token));
+      chai.assert.isFalse(TokenValidator.isExpired(token));
     });
   });
 
   describe("#isExpiring()", () => {
     it("Should return true if token has less than one month to expire.", () => {
-      chai.assert.isTrue(TokenValidation.isExpiring(expiringToken));
+      chai.assert.isTrue(TokenValidator.isExpiring(expiringToken));
     });
     it("Should return false if token has more than one month to expire", () => {
-      chai.assert.isTrue(TokenValidation.isExpiring(token));
+      chai.assert.isFalse(TokenValidator.isExpiring(token));
     });
   });
 });
