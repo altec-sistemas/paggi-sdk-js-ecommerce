87ea857276769abae76737ec75063e12a577a46f
= <=>
2018-10-21 00:20:20 -0300

    Mudança para var

--- SDK/tokenValidation.js
+++ SDK/tokenValidation.js
@@ -1,9 +1,9 @@
-const jwtDecode = require("jwt-decode");
+var jwtDecode = require("jwt-decode");
 
 function TokenValidation() {
   helper = (token, subtract) => {
-    const decodedToken = jwtDecode(token);
-    const date = new Date(decodedToken.exp);
+    var decodedToken = jwtDecode(token);
+    var date = new Date(decodedToken.exp);
     todayDate = new Date().valueOf() / 1000;
     return +todayDate > +date - subtract;
   };
@@ -28,35 +28,3 @@ TokenValidation.prototype.isExpired = token => helper(token, 0);
 TokenValidation.prototype.isExpiring = token => helper(token, 2592000);
 
 module.exports = TokenValidation;
-
-/*
-class TokenValidation {
-  isValid(token) {
-    let isPartnerSet;
-    try {
-      const decodedToken = jwtDecode(token);
-      isPartnerSet =
-        typeof decodedToken.permissions[0].partner_id !== "undefined";
-    } catch (err) {
-      isPartnerSet = false;
-    } finally {
-      return isPartnerSet;
-    }
-  }
-
-  isExpired(token) {
-    return this.helper(token, 0);
-  }
-
-  isExpiring(token) {
-    return this.helper(token, 2592000);
-  }
-
-  helper(token, subtract) {
-    const decodedToken = jwtDecode(token);
-    const date = new Date(decodedToken.exp);
-    const todayDate = new Date().valueOf() / 1000;
-    return +todayDate > +date - subtract;
-  }
-}
-*/
