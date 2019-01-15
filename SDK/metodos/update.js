a5af4c990206bcaa8a2af72dab504984ae998e8b
= <=>
2019-01-15 13:57:34 -0200

    COrreção update

--- SDK/metodos/update.js
+++ SDK/metodos/update.js
@@ -2,8 +2,8 @@ var makeRequest = require("./util");
 var manageResponse = require("./responseManagement");
 
 var del = (classe, params, objectId) => {
-    response = makeRequest(classe, "UPDATE", params, [], objectId);
-    return manageResponse(response);
+  response = makeRequest(classe, "PUT", params, [], objectId);
+  return manageResponse(response);
 };
 
 module.exports = del;
