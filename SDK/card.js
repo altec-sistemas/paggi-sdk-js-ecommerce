31c9527210caab53f8b0d55651e68d2901a07f8d
Daniel Galdino <daniel.galdino@altec.ws>
2020-07-06 13:48:32 -0300

    add find on card methods

--- SDK/card.js
+++ SDK/card.js
@@ -1,7 +1,9 @@
 var creator = require("./metodos/create");
 var deleter = require("./metodos/delete");
+var finder = require("./metodos/find");
 
 module.exports = {
   create: params => creator("cards", params),
-  del: id => deleter("cards", id)
+  del: id => deleter("cards", id),
+  find: document => finder("cards", document)
 };
